8e08a43aeccfe83b0f5108754e18fcf541c69f75
Parrish Williamson <williamsonparrish@gmail.com>
2020-10-15 09:44:34 -0400

    tuples are immutable

--- hello_world.py
+++ hello_world.py
@@ -16,3 +16,48 @@ print(f'Hello, his name is {name}') #formatted string literals
 name = 'Sam'
 age = 3
 print(f'{name} is {age} years old.')
+
+my_list = [1,2,3]
+my_list = ['STRING',100,23.2]
+print(len(my_list))
+
+mylist = ['one','two','three']
+print(mylist[1:])
+another_list = ['four','five']
+new_list = mylist + another_list
+new_list[0] = 'ONE ALL CAPS'
+print(new_list)
+new_list.append('six')
+# .append() adds items to the end of a list
+print(new_list)
+print(new_list.pop())
+# .pop() removes an item from the end of a list
+print(new_list)
+new_list.pop(0)
+print(new_list)
+
+new_list = ['a','e','x','b','c']
+num_list = [4,1,8,3]
+new_list.sort()
+print(new_list)
+# A None object is a return value for something that doesn't have a value
+num_list.sort()
+print(num_list)
+num_list.reverse()
+print(num_list)
+
+my_dict = {'key1' : 'value1','key2':'value2'} #dictionary
+print(my_dict)
+prices_lookup = {'apple':2.99,'oranges':1.99,'milk':5.99}
+print(prices_lookup['apple'])
+d = {'k1':123,'k2':[0,1,2],'k3':{'insideKey':100}}
+print(d['k3']['insideKey'])
+print(d['k2'])
+
+d = {'key1':['a','b','c']}
+my_list = d['key1']
+letter = my_list[2]
+print(letter.upper())
+print(d['key1'][2].upper())
+d['key3']=100
+print(d)
