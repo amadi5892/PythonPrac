ca1306b91432e44eaf1896f2b1e7882a923690df
Parrish Williamson <williamsonparrish@gmail.com>
2020-10-19 15:08:38 -0400

    completed statements assessments

--- loop.py
+++ loop.py
@@ -1,10 +1,118 @@
-mylist = [1,2,3,4,5,6,7,8,9,10]
-for jelly in mylist:
-    print('hello')
+# mylist = [1,2,3,4,5,6,7,8,9,10]
+# for jelly in mylist:
+#     print('hello')
 
-for num in mylist:
-    #Check for even
-    if num % 2 == 0:
-        print(f'Even Number: {num}')
-    else:
-        print(f'Odd Number: {num}')
+# for num in mylist:
+#     #Check for even
+#     if num % 2 == 0:
+#         print(f'Even Number: {num}')
+#     else:
+#         print(f'Odd Number: {num}')
+
+# list_sum = 0
+
+# for num in mylist:
+#     list_sum = list_sum + num
+
+# print(list_sum)
+
+# mystr = 'Hello World'
+# for letter in 'Hello World':
+#     print(letter)
+
+# tup = (1,2,3)
+
+# for item in tup:
+#     print(item)
+
+# mylist = [(1,2,3),(4,5,6),(7,8,9)]
+
+# print(len(mylist))
+
+# for item in mylist:
+#     print(item)
+
+# # for (a,b) in mylist:
+# #     print(a)
+# #     print(b)
+
+# d = {'k1': 1, 'k2':2, 'k3': 3}
+
+# for key,value in d.items():
+#     print(key, ':', value)
+
+# x = 0
+
+# while x < 5:
+#     print(f'The curretn value of x is {x}')
+#     x += 1
+# else:
+#     print("X IS NOT LESS THAN 5")
+
+# x = [1,2,3]
+# for item in x:
+#     pass
+# print('end of my script')
+
+# mystr = 'Sammy'
+# for letter in mystr:
+#     if letter == 'a':
+#         continue
+#     print(letter)
+
+# x = 0
+# while x < 5:
+
+#     if x == 2:
+#         break
+#     print(x)
+#     x += 1
+
+# mylist = [1,2,3]
+# for num in range(0,10,2):
+#     print(num)
+
+# index_count = 0
+# word = 'abcde'
+# for letter in word:
+#     print(word[index_count])
+#     index_count += 1
+
+# word = 'abcde'
+# for index,letter in enumerate(word):
+#     print(index)
+#     print(letter)
+#     print('\n')
+
+# mylist1 = [1,2,3,4,5]
+# mylist2 = ['a','b','c']
+# mylist3 = [100,200,300]
+
+# for item in zip(mylist1,mylist2,mylist3):
+#     print(item)
+
+# print('a' in 'a world')
+
+# from random import shuffle
+
+# mylist = [1,2,3,4,5,6,7,8,9,10]
+# random_list = shuffle(mylist)
+# print(type(random_list))
+
+# from random import randint
+# print(randint(0,100))
+
+# result = input('What is your name? ')
+
+mystring = 'hello'
+mylist = []
+
+for letter in mystring:
+    mylist.append(letter)
+    
+mylist = [letter for letter in mystring]
+print(mylist)
+
+celcius = [0,10,20,34.5]
+fahrenheit = [( (9/5)*temp + 32) for temp in celcius]
+print(fahrenheit)
---
+++ statements_assessments.py
@@ -0,0 +1,37 @@
+# st = 'Print only the words that start with s in this sentence'
+# for i in st.split():
+#     if i.startswith('s'):
+#         print(i)
+
+# print('\n')
+
+# for num in range(0,11):
+#     print(num)
+
+# print('\n')
+
+# mylist = []
+# for num in range(0,51):
+#     if num % 3 == 0:
+#         mylist.append(num)
+# print(mylist)
+
+# st = 'Print every word in this sentence that has an even number of letters'
+# st.split()
+# for i in st.split():
+#     if len(i) % 2 == 0:
+#         print(i)
+
+# for num in range(0,101):
+#     if num % 3 == 0 and num % 5 == 0:
+#         print('FizzBuzz')
+#     elif num % 3 == 0:
+#         print('Fizz')
+#     elif num % 5 == 0:
+#         print('Buzz')
+#     else:
+#         print(num)
+
+st = 'Create a list of the first letters of every word in this string'
+for i in st.split():
+    print(i[0])
