0da270dd31f04cbf87b413eb10df74d1f40d8f5e
Parrish Williamson <williamsonparrish@gmail.com>
2020-11-06 18:00:52 -0500

    methods and function homework updated

---
+++ methods_func_hw.py
@@ -0,0 +1,16 @@
+# # Volume of Sphere
+# import math
+# def vol(rad):
+#     return (4/3)*math.pi*(rad ** 3)
+
+# print(vol(2))
+
+
+# Write a function that checks whether a number is in a given range (inclusive of high and low)
+
+# def ran_check(num,low,high):
+#     if low < num < high:
+#         return f'{num} is in the range between {low} and {high}'
+
+# print(ran_check(5,2,7))
+
